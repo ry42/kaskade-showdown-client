ecb6031302df543ccd92f85d4fe6d224804c738c
ry <ryr4242@gmail.com>
2025-03-30 04:23:25 -0500

    Final minisprite update for now

--- play.pokemonshowdown.com/src/battle-dex-data.ts
+++ play.pokemonshowdown.com/src/battle-dex-data.ts
@@ -821,7 +821,7 @@ const BattlePokemonIconIndexes: {[id: string]: number} = {
 	blurruncharged: 1800 + 4,
 	droutdry: 1800 + 5,
 	tamroastf: 1800 + 6,
-	weepollenf: 1608 + 69,
+	weepollenf: 1800 + 7,
 	bearvoyancewhite: 1800 + 8,
 };
 
